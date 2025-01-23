21eaa13534fe5f19a9c250dff92502f4ceb15175
Resben <bendavidhawkins@gmail.com>
2025-01-23 19:59:21 +0800

    [Fix] Direction drift

--- scenes/agents/player.gd
+++ scenes/agents/player.gd
@@ -16,7 +16,7 @@ func init_player():
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
 	
-	direction = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), Input.get_action_strength("down") - Input.get_action_strength("up"))
+	direction = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), Input.get_action_strength("down") - Input.get_action_strength("up")).normalized()
 
 	if velocity.length() > 1:
 		velocity = velocity.normalized()
